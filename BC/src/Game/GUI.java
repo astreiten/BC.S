0a45548ebe61b288ffaaec5b98ea431ed9f99ac3
AgustinStr <agus.stragua@gmail.com>
2016-09-11 16:06:25 -0300

    Por ahora anda

--- BC/src/Game/GUI.java
+++ BC/src/Game/GUI.java
@@ -36,8 +36,9 @@ public class GUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GUI() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 832, 832);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
