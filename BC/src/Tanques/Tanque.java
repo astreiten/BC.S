714ba221b800fdc07676c426d9716e07c2d63327
AgustinStr <agus.stragua@gmail.com>
2016-09-15 10:12:50 -0300

    Nuevas imagenes .gif

--- BC/src/Tanques/Tanque.java
+++ BC/src/Tanques/Tanque.java
@@ -8,10 +8,7 @@ import Game.GameObject;
 public abstract class Tanque extends GameObject{
 	public Tanque(Celda celdita) {
 		super(celdita);
-		image[0] = new ImageIcon(this.getClass().getResource("/Imagenes/Tanque.gif"));
-		image[1] = new ImageIcon(this.getClass().getResource("/Imagenes/JugadorAbajo.png"));
-		image[2] = new ImageIcon(this.getClass().getResource("/Imagenes/JugadorIzquierda.png"));
-		image[3] = new ImageIcon(this.getClass().getResource("/Imagenes/JugadorDerecha.png"));
+		
 	}
 	protected int resistencia;
 	protected int vel_dispa;
--- BC/src/Tanques/TanqueJugador.java
+++ BC/src/Tanques/TanqueJugador.java
@@ -1,10 +1,16 @@
 package Tanques;
 
+import javax.swing.ImageIcon;
+
 import Game.Celda;
 
 public class TanqueJugador extends Tanque{
 	public TanqueJugador(Celda celdita) {
 		super(celdita);
+		image[0] = new ImageIcon(this.getClass().getResource("/Imagenes/Tanque.gif"));
+		image[1] = new ImageIcon(this.getClass().getResource("/Imagenes/JugadorAb.gif"));
+		image[2] = new ImageIcon(this.getClass().getResource("/Imagenes/JugadorIzq.gif"));
+		image[3] = new ImageIcon(this.getClass().getResource("/Imagenes/JugadorDer.gif"));
 		
 	}
 	protected int nivel;
