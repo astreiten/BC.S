ee3d4659a69900f1833a0946ff17abc0cfd3b093
AgustinStr <agus.stragua@gmail.com>
2016-10-26 10:19:57 -0300

    Revert "No prestar atencion"
    This reverts commit 9e83a82d3654c838bf47beb0ba13a38c67353382.

--- Proyecto2/Proyecto2/testingScript.sh
+++
@@ -1,4 +0,0 @@
-cd
-cd Escritorio/Proyecto2
-gcc evaluar.c lista.c pila.c -o evaluar
-./evaluar -i /home/alumno/Escritorio/Proyecto2/archivo_entrada  -o /home/alumno/Escritorio/Proyecto2/archivo_salida
