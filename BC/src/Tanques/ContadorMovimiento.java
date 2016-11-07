dd1ae9afebddaa49583081574792ea14a30121f7
AgustinStr <agus.stragua@gmail.com>
2016-11-07 10:11:41 -0300

    Mov mas mejorado

--- BC/src/Tanques/ContadorMovimiento.java
+++ BC/src/Tanques/ContadorMovimiento.java
@@ -6,7 +6,7 @@ public class ContadorMovimiento extends Thread {
 
 	private TanqueJugador tan;
 	private boolean puedo = false;
-	private int contador= 0;
+	private int contador = 0;
 
 	public ContadorMovimiento(TanqueJugador tanq) {
 		tan = tanq;
@@ -14,42 +14,44 @@ public class ContadorMovimiento extends Thread {
 
 	public void run() {
 		while (true) {
+
+		
+			
 			if (puedo) {
 				try {
-					Thread.sleep(150);
+					Thread.sleep(100 * tan.getSleep());
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 
-				
 				contador++;
-
 				int x = tan.getCelda().getFila();
 				int y = tan.getCelda().getCol();
 
 				switch (tan.getDir()) {
 				case 0: // Arriba
 
-					tan.getGrafico().setBounds(y * 50, x * 50 - 4, 50, 50);
+					tan.getGrafico().setBounds(y * 50, x * 50 - 6, 50, 50);
 					break;
 
 				case 1: // Abajo
 
-					tan.getGrafico().setBounds(y * 50 , x * 50 + 4 , 50, 50);
+					tan.getGrafico().setBounds(y * 50, x * 50 + 6, 50, 50);
 					break;
 
 				case 2: // Izquierda
 
-					tan.getGrafico().setBounds(y * 50 - 4, x * 50, 50, 50);
+					tan.getGrafico().setBounds(y * 50 - 6, x * 50, 50, 50);
 					break;
 
 				case 3: // Derecha
-					tan.getGrafico().setBounds(y * 50 + 4, x * 50 , 50, 50);
+					tan.getGrafico().setBounds(y * 50 + 6, x * 50, 50, 50);
 					break;
 				}
+
 				tan.setMovilidad(true);
 				if (contador == tan.getSleep()) {
-					
+
 					puedo = false;
 				}
 			}
--- BC/src/Tanques/Nivel2.java
+++ BC/src/Tanques/Nivel2.java
@@ -9,7 +9,7 @@ public class Nivel2 extends State{
 		tanque.cargarImagen(1,"JugadorLvl2Down.gif");
 		tanque.cargarImagen(2,"JugadorLvl2Left.gif");
 		tanque.cargarImagen(3,"JugadorLvl2Right.gif");
-		velocidad = 5;
+		velocidad = 2;
 	}
 
 }
